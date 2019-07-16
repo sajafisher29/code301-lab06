ab2ac3cabec2c592067e15c9ec01d1442d5e628f
sajafisher29 <sajafisher@gmail.com>
2019-07-16 11:02:02 -0700

    weather feature completed

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -6,6 +8,11 @@ function Location(query, geoData) {
   this.city_query = query;
   this.latitude = geoData.results[0].geometry.location.lat;
   this.longitude = geoData.results[0].geometry.location.lng;
+}
+
+function Weather(forecast, time) {
+  this.forecast = forecast;
+  this.time = time;
 }
 
 app.get('/location', (req, res) => { 
@@ -18,6 +25,16 @@ app.get('/location', (req, res) => {
     res.status(400).send({'error': err })
   }
 });
+
+app.get('/weather', (req, res) => {
+  const weatherArr = [];
+
+  const darkskyData = require('./data/darksky.json');
+  darkskyData.daily.data.forEach( day => {
+    weatherArr.push(new Weather(day.summary, new Date(day.time).toString()));
+  })
+  res.send(weatherArr);
+})
 
 app.listen(PORT, () => { 
   console.log('Listening to PORT: ' + PORT);
